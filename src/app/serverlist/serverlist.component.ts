56e85f1dfc282b3a3aa3382bb803b3ff1f699605
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-05-22 15:53:20 -0500

    added ngIf paragraph to match

--- src/app/serverlist/serverlist.component.ts
+++ src/app/serverlist/serverlist.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerlistComponent implements OnInit {
 
-  allowServerCreation = false;
+  allowServerCreation = false
   serverCreationStatus = 'No server created'
   serverName = 'test-server'
+  servers = ['test-server 1', 'test-server 2']
+  serverCreated = false
 
 
   constructor() {
@@ -24,5 +26,7 @@ export class ServerlistComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Created server: ' + this.serverName
+    this.serverCreated = true
+    this.servers.push(this.serverName)
   }
 }
